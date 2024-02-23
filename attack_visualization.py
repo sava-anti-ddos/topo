7bdfb629d0d2403f12dccab656746d4e794d8a69
whh <whhxd@outlook.com>
2024-02-23 14:07:11 +0800

    visualization adjustment

--- attack_visualization.py
+++ attack_visualization.py
@@ -25,13 +25,17 @@ dns_responses_grouped = dns_responses.groupby('time').agg(
 
 # Plot query bytes and response bytes per second over time
 fig, ax = plt.subplots(figsize=(12, 6))
-ax.plot(dns_queries_grouped['time'], dns_queries_grouped['queries_total_size'], label='Query', linewidth=1, color='blue')
-ax.fill_between(dns_queries_grouped['time'], dns_queries_grouped['queries_total_size'], color='blue', alpha=0.1)
-ax.plot(dns_responses_grouped['time'], dns_responses_grouped['responses_total_size'], label='Response', linewidth=1, color='orange')
-ax.fill_between(dns_responses_grouped['time'], dns_responses_grouped['responses_total_size'], color='orange', alpha=0.1)
+ax.plot(dns_queries_grouped['time'], dns_queries_grouped['queries_total_size'], 
+        label='Query', linewidth=1, color='blue', 
+        marker='o', markersize=4, markerfacecolor='blue', markeredgecolor='blue')
+# ax.fill_between(dns_queries_grouped['time'], dns_queries_grouped['queries_total_size'], color='blue', alpha=0.1)
+ax.plot(dns_responses_grouped['time'], dns_responses_grouped['responses_total_size'], 
+        label='Response', linewidth=1, color='orange',
+        marker='v', markersize=4, markerfacecolor='orange', markeredgecolor='orange')
+# ax.fill_between(dns_responses_grouped['time'], dns_responses_grouped['responses_total_size'], color='orange', alpha=0.1)
 ax.set_xlabel('Time (Minute:Second)')
 ax.set_ylabel('Bytes Per Second')
-ax.set_title('Query bytes and response bytes per second over time')
+ax.set_title('Query Bps and response Bps over time')
 ax.legend(loc='upper right')
 
 # set the ticks
@@ -48,13 +52,13 @@ ax.set_yticks(np.arange(0, max(dns_queries_grouped['queries_total_size'].max(),
 ax.xaxis.set_major_formatter(matplotlib.dates.DateFormatter('%M:%S'))
 
 # only show the bottom spine
-ax.spines['top'].set_visible(False)
-ax.spines['right'].set_visible(False)
-ax.spines['left'].set_visible(False)
+# ax.spines['top'].set_visible(False)
+# ax.spines['right'].set_visible(False)
+# ax.spines['left'].set_visible(False)
 
 ax.yaxis.grid(color='gray', linewidth=0.2)
-ax.tick_params(axis='y', which='both', length=0, pad=20)
-ax.get_xaxis().get_major_ticks()[0].set_visible(False)
+# ax.tick_params(axis='y', which='both', length=0, pad=20)
+# ax.get_xaxis().get_major_ticks()[0].set_visible(False)
 
 # save the plot to a file
 plt.savefig('audit/attack_visualization.png')
