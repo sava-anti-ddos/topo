64432c0e49537b3f2ba5b59db6c0f67f09c49595
whh <whhxd@outlook.com>
2024-02-26 18:26:03 +0800

    random traffic generate

---
+++ random_http_request.py
@@ -0,0 +1,38 @@
+# each host send http requests to the other hosts, and wait a random time before sending the next request
+
+import random
+import time
+import requests
+import sys
+
+# send http request to the target
+def send_http_request(target):
+    try:
+        response = requests.get(target)
+        print(response)
+    except Exception as e:
+        print(e)
+
+if __name__ == '__main__':
+    # check the param
+    if len(sys.argv) != 2:
+        print('Usage: python3 random_http_request.py <self_ip>')
+        sys.exit(1)
+
+    # self ip
+    self_ip = sys.argv[1]
+    # target list
+    targets = ['50.50.10.10',
+               '20.20.30.10',
+               '20.20.60.10',
+               '30.30.10.10',
+               '40.40.10.10',]
+    # loop forever
+    while True:
+        # random pick a target except self
+        target = random.choice(targets)
+        if target != self_ip:
+            # send http request to the target
+            send_http_request(f'http://{target}')
+            # random sleep
+            time.sleep(random.randint(1, 5))
